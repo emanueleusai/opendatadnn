0d31d27c76a12c5d68a0af7bc16bf5270bbdfc75
Emanuele Usai <ema2487@gmail.com>
2019-02-27 21:35:31 +0100

    m. and s. files

---
+++ crab_step0_ttbarOD_Sitong.py
@@ -0,0 +1,29 @@
+from CRABClient.UserUtilities import config, getUsernameFromSiteDB
+config = config()
+
+the_name='step0_ttbarOD_Sitong'
+config.General.requestName = the_name
+config.General.workArea = 'crab_projects'
+config.General.transferOutputs = True
+config.General.transferLogs = True
+
+config.JobType.pluginName = 'PrivateMC'
+config.JobType.psetName = 'step0_ttbarOD_Sitong.py'
+config.JobType.maxMemoryMB = 2500
+config.JobType.numCores = 1
+
+#config.Data.inputDataset = '/SinglePiPt100Eta1p6_2p8/PhaseIITDRFall17DR-noPUFEVT_93X_upgrade2023_realistic_v2-v1/GEN-SIM-RECO'
+#config.Data.inputDBS = 'global'
+config.Data.splitting = 'EventBased'
+config.Data.unitsPerJob = 200
+config.Data.outLFNDirBase = '/store/group/lpcml/'# % (getUsernameFromSiteDB())
+config.Data.publication = False
+config.Data.outputDatasetTag = the_name
+config.Data.ignoreLocality = True
+config.Data.totalUnits = 1000000
+
+config.Site.storageSite = 'T3_US_FNALLPC'
+#config.Site.ignoreGlobalBlacklist = True
+config.Site.whitelist = ['T2_US_*']
+#config.Site.whitelist = ['T3_US_FNALLPC']
+#config.Site.blacklist = ['T2_US_Florida']
